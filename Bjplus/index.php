9de9efee8a7f3ec6ad80da38625dbcef9d9308c6
kevin <kevspano@gmail.com>
2024-01-26 11:07:27 +0100

    ajout Bjplus

---
+++ Bjplus/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Bjplus, spécialiste en terrassement, assainissement, pose de canalisation et VRD à accès difficile. Expert en maçonnerie, Bjplus propose également la location de grue, nacelle, et la pose de bennes. Basée à La Serre les Sapins, en Franche-Comté.">
+    <meta name="keywords" content="Bjplus, terrassement, assainissement, canalisation, VRD, chantier à accès difficile, maçonnerie, location de grue, location de nacelle, pose de bennes, Serre les Sapins, Franche-Comté, travaux publics, génie civil, excavation, entreprise de construction, TP, travaux de voirie, piscine, location d'équipement, engins de chantier, travaux d'aménagement, drainage, travaux de démolition">
+    <meta name="author" content="Spano Kevin">
+    <meta name="subject" content="Entreprise spécialisée en travaux de terrassement, assainissement, pose de canalisation et maçonnerie à La Serre les Sapins, Franche-Comté.">
+    <meta name="robots" content="index,follow">
+
+    <!-- Link -->
+    <link rel="icon" href="img/site/Favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" sizes="180x180" href="img/site/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="img/site/android-icon.png">
+
+    <title>Bjplus</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <header class="hero-banner">
+        <nav class="navbar">
+            <div class="container">
+                <a class="navbar-brand" href="#home"><img id="logo" src="img/site/logo BJPLUS.png" alt="Logo Bjplus Terrassement"></a>
+                <button class="burger" type="button">
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                </button>
+                <div class="nav-navbar">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#home">Accueil</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#about">À Propos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#prestation">Prestations</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#loc">Location</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#contact">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <!-- JavaScript -->
+    <script src="js/script.js"></script>
+
+    <!-- Script pour rafraîchir la page toutes les 5 secondes -->
+    <!-- <script>
+        setInterval(function() {
+            location.reload();
+        }, 5000);
+    </script> -->
+</body>
+
+</html>
